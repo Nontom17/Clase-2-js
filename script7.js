9fe3d97764d892c466851ccb76d0df7af54f44d6
Tomas <tomitomnu@gmail.com>
2026-09-23 16:00:34 -0300

    ejercicio 7 y 8

---
+++ script7.js
@@ -0,0 +1,4 @@
+let celsius = Number(prompt("Ingrese los grados"));
+let farenheit = celsius*9/5+32;
+
+console.log(`Los grados en farenheit son: ${farenheit} y en celsius: ${celsius}`);
---
+++ script8.js
@@ -0,0 +1,3 @@
+let numero = Number(prompt("Ingrese un numero"));
+
+console.log(`El resto al dividirlo por 7 es: ${numero%7}`);
