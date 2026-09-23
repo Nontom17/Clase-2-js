6ac7e5a41f12308d728a155a2290c9e360a7356c
Tomas <tomitomnu@gmail.com>
2026-09-23 15:31:24 -0300

    Primeros 4 ejercicios de la clase

---
+++ script1.js
@@ -0,0 +1,24 @@
+//Ejercicio 1
+
+let nombre = prompt("¿Cual es tu nombre?");
+
+console.log("¡Hola, "+ nombre + "!");
+
+//Ejercicio 2
+
+let a = Number(prompt("Ingesa un numero"));
+let b = Number(prompt("Ingesa otro numero"));
+
+console.log(`La suma es: ${a+b}`);
+
+//Ejercicio 3
+
+let edad = Number(prompt("Ingresa tu edad"));
+let estatura = Number(prompt("Ingresa tu estatura"));
+
+console.log(`${nombre} tiene ${edad} y mide ${estatura}`);
+
+//Ejercicio 4
+
+let base = Number(prompt("Ingrese la base del triangulo"));
+let altura = Number(prompt("ingrese la altura del triangulo"));
